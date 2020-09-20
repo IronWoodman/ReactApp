e4922532f944cf93ce8b84b97e0e279869a23f32
IronWoodman <snkobyakov@yandex.ru>
2020-09-20 13:04:23 +0500

    Trying to load osrm library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './App.css';
+const OSRM = require('osrm');
 
 function App() {
+  console.log('=version', OSRM.version);
+  // const osrm = new OSRM('./static/kor.osrm');
+
   return (
     <div className="App">
       <header className="App-header">
